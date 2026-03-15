661308bfd2d8c9b3fdfc5adc6cc64b281b51ee25
Sanchitjadhav007 <jadhavsanchit835@gmail.com>
2026-03-15 14:24:23 +0530

    Update review.js

--- review.js
+++ review.js
@@ -9,7 +9,7 @@ document.getElementById("reviewForm").addEventListener("submit", async function(
   };
 
   try {
-    await fetch("cafe-website-7ea0aivk4-sanchitjadhav007s-projects.vercel.app/api/review", {
+    await fetch("https://cafe-website-r8vm92xio-sanchitjadhav007s-projects.vercel.app/api/review", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(review)
@@ -27,7 +27,7 @@ document.getElementById("reviewForm").addEventListener("submit", async function(
 // Function to load all reviews
 async function loadReviews() {
   try {
-    const response = await fetch("cafe-website-7ea0aivk4-sanchitjadhav007s-projects.vercel.app/api/review", {
+    const response = await fetch("https://cafe-website-r8vm92xio-sanchitjadhav007s-projects.vercel.app/api/review", {
       mode: 'cors' // Added mode: 'cors' here
     });
     const reviews = await response.json();
